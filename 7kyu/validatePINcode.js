0a57a254864888b726d586d5c5397c04056e2b11
michelle armstrong <linmicarm@gmail.com>
2026-06-30 07:46:03 -0400

    Add validatePIN function for PIN code validation
    Implement validatePIN function to check for valid 4 or 6 digit PIN codes.

---
+++ 7kyu/validatePINcode.js
@@ -0,0 +1,14 @@
+/*
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+
+const validatePIN = pin => {
+  return (pin.length === 4 || pin.length === 6) && [...pin].every(c => c >= "0" && c <= "9");
+}
