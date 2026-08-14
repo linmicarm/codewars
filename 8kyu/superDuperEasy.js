d2b5e9f803e50f398b419a96edb5fe8977986b5d
michelle armstrong <linmicarm@gmail.com>
2026-08-14 09:14:32 -0400

    Refactor problem function to arrow function syntax

--- 8kyu/superDuperEasy.js
+++ 8kyu/superDuperEasy.js
@@ -2,9 +2,4 @@
 Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 */
 
-function problem(x) {
-    if (typeof x === 'string') {
-        return "Error";
-    }
-    return (x * 50) + 6;
-}
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
