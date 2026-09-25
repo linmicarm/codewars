01cce0ae2ce9fc3c997fa821d96efcb46f442fdf
michelle armstrong <linmicarm@gmail.com>
2026-09-25 08:47:27 -0400

    Add function to find the average of an array
    Implement a function to calculate the mean of an array of numbers.

---
+++ 8kyu/arrayMean.js
@@ -0,0 +1,33 @@
+/**
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+
+Information
+To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4
+*/
+
+function findAverage(nums) {
+
+    // Start our running total
+    let total = 0;
+
+    // Loop through the array
+    for (let i = 0; i < nums.length; i++) {
+
+        // Add the current number to total
+        total += nums[i];
+    }
+
+    // Return the mean
+    return total/nums.length;
+}
